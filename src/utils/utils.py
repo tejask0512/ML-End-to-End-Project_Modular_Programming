edb6e8d31c7090b3c27b20e632a61b024830a471
tejask0512 <tejask0512@gmail.com>
2025-04-10 16:17:20 +0530

    data-transformation

---
+++ src/utils/utils.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+import numpy as np 
+import pandas as pd
+import dill
+import pickle
+from src.exceptions.exception import ProjectException
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise ProjectException(e, sys)
