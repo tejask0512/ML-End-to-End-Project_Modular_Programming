72a17413d5acdde3314d332d7dcdf4daad75d6f5
tejask0512 <tejask0512@gmail.com>
2025-04-10 13:05:03 +0530

    EDA

--- src/exceptions/exception.py
+++ src/exceptions/exception.py
@@ -1,15 +1,22 @@
 import sys
-from src.logging import logger
+from src.logging.logger import logging
 
 class ProjectException(Exception):
     def __init__(self,error_message,error_details:sys):
         self.error_message = error_message
         _,_,exc_tb = error_details.exc_info()
-        error_message=""
+        
         
         self.lineno=exc_tb.tb_lineno
         self.file_name=exc_tb.tb_frame.f_code.co_filename 
+    def __str__(self):
     
         return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
         self.file_name, self.lineno, str(self.error_message))
  
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Zero division error")
+        raise ProjectException(e,sys)
--- src/logging/logger.py
+++ src/logging/logger.py
@@ -2,9 +2,10 @@ import logging
 import os
 from datetime import datetime
 
+print(os.getcwd())
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+logs_path=os.path.join(os.getcwd(),"./logs",LOG_FILE)
 os.makedirs(logs_path,exist_ok=True)
 
 LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
